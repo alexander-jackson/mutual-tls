abc2ae97961c6508eecccd1d067715d2c2d7fc5a
Alexander Jackson <alexanderjackson@protonmail.com>
2024-10-16 19:32:27 +0100

    refactor: tidy up code slightly

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ use hyper::body::Incoming;
 use hyper::service::service_fn;
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use hyper_util::server::conn::auto::Builder;
+use itertools::Itertools;
 use rustls::pki_types::pem::PemObject;
-use rustls::pki_types::PrivateKeyDer;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use rustls::server::WebPkiClientVerifier;
 use rustls::{RootCertStore, ServerConfig};
 use tokio::net::TcpListener;
@@ -20,6 +21,10 @@ use tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
+
+const ROOT_CERT_PATH: &str = "certs/ca.crt";
+const CERT_CHAIN_PATH: &str = "certs/localhost.bundle.crt";
+const KEY_PATH: &str = "certs/localhost.key";
 
 fn setup() -> Result<()> {
     color_eyre::install()?;
@@ -51,6 +56,23 @@ fn initialise_root_cert_store<P: AsRef<Path>>(path: P) -> Result<RootCertStore> 
     Ok(store)
 }
 
+fn get_server_credentials<C: AsRef<Path>, K: AsRef<Path>>(
+    chain_path: C,
+    key_path: K,
+) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>)> {
+    let chain_path = chain_path.as_ref();
+    let key_path = key_path.as_ref();
+
+    let mut rdr = BufReader::new(File::open(chain_path)?);
+    let server_certificate = rustls_pemfile::certs(&mut rdr).try_collect()?;
+    tracing::info!(path = ?chain_path, "read a certificate chain for the server");
+
+    let private_key = PrivateKeyDer::from_pem_file(key_path)?;
+    tracing::info!(path = ?key_path, "read a private key for the server");
+
+    Ok((server_certificate, private_key))
+}
+
 async fn root(req: Request<Incoming>) -> Result<Response<String>, hyper::Error> {
     tracing::info!(?req, "handling a request");
 
@@ -61,20 +83,14 @@ async fn root(req: Request<Incoming>) -> Result<Response<String>, hyper::Error> 
 async fn main() -> Result<()> {
     setup()?;
 
-    let store = initialise_root_cert_store("certs/ca.crt")?;
+    let store = initialise_root_cert_store(ROOT_CERT_PATH)?;
     let verifier = WebPkiClientVerifier::builder(Arc::new(store)).build()?;
 
-    let server_certificate = rustls_pemfile::certs(&mut BufReader::new(File::open(
-        "certs/localhost.bundle.crt",
-    )?))
-    .filter_map(Result::ok)
-    .collect();
-
-    let private_key = PrivateKeyDer::from_pem_file("certs/localhost.key")?;
+    let (certificate_chain, private_key) = get_server_credentials(CERT_CHAIN_PATH, KEY_PATH)?;
 
     let config = ServerConfig::builder()
         .with_client_cert_verifier(verifier)
-        .with_single_cert(server_certificate, private_key)?;
+        .with_single_cert(certificate_chain, private_key)?;
 
     let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3000);
 
